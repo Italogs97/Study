ffae0b59d3d42986d416298f7ef71c2623c66e52
italogs97 <italogs97@hotmail.com>
2024-11-18 19:46:59 -0300

    .

---
+++ Java/src/estrutura-de-repeticao/ExemploCarneirinhos.java
@@ -0,0 +1,11 @@
+// ExemploFor.java
+
+
+
+public class ExemploCarneirinhos {
+	public static void main(String[] args) {
+		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println("Contando carneirinhos" + carneirinhos);
+		}
+	}
+}
